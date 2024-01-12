17db86ab76e4eb052966dc9c76d57e7a43770688
Padmanabh Malwade <padmanabhmalwade@gmail.com>
2024-01-12 11:58:34 +0530

    Modified the Image Domains to Image Remote Patterns

--- next.config.js
+++ next.config.js
@@ -2,11 +2,29 @@
 
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "**",
+      },
     ],
+
+    // domains: [
+    //   "res.cloudinary.com",
+    //   "lh3.googleusercontent.com",
+    //   "avatars.githubusercontent.com",
+    // ],
   },
   experimental: {
     swcPlugins: [["next-superjson-plugin", {}]],
